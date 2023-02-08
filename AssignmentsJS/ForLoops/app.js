1a08fb7849158d21107141b74e4ac8e0d2389f19
MuhammedUsmanKhan <ukteck0310@gmail.com>
2023-02-08 17:43:39 +0500

    ForLoop

--- AssignmentsJS/ForLoops/app.js
+++ AssignmentsJS/ForLoops/app.js
@@ -23,3 +23,12 @@ for(var i = 1 ; i <= num2 ;i++)
 {
  document.write(num1 + " x " + i + " = " + num1*i + "<br>");
 }
+// 5. Write a program to print items of the following array using for
+// loop:
+var fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+
+for(var i = 0 ; i < 5 ; i++)
+{
+document.write(fruits[i] + "<br>")
+document.write("Element at index " + i + fruits[i] + "<br>")
+}
